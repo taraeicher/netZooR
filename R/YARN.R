e4fda96c93d5d26995359f64b1cf05c55e4168be
Tara Eicher <taraeicher@yahoo.com>
2025-06-27 14:11:42 -0400

    Added curl download

--- R/YARN.R
+++ R/YARN.R
@@ -23,9 +23,8 @@
 #' u <- 'https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/'
 #' bladder <- paste0(u, 'yarn/bladder.rdata')
 #' skin <- paste0(u, 'yarn/skin.rdata')
-#' print(getwd())
-#' download.file(bladder, destfile='data/bladder.rdata')
-#' download.file(skin, destfile='data/skin.rdata')
+#' download.file(bladder, destfile='data/bladder.rdata', method = "curl")
+#' download.file(skin, destfile='data/skin.rdata', method = "curl")
 #' data(skin)
 #' # subsetting and changing column name just for a silly example
 #' skin <- skin[1:10,]
